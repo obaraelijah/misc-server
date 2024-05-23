36f1906210994873f308658a3a72c57e2f9f7338
obaraelijah <elijahobara357@gmail.com>
2024-05-23 11:30:48 +0300

    fix:  s3 error handling issue

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,4 @@
+use aws_sdk_s3::{error::SdkError, operation::list_objects_v2::ListObjectsV2Error};
 use serde::Serialize;
 use thiserror::Error;
 
@@ -5,6 +6,14 @@ use thiserror::Error;
 pub enum ServerError {
     #[error("Failed to retrieve objects: {message}")]
     ListObjects { message: String },
+}
+
+impl From<SdkError<ListObjectsV2Error>>  for ServerError {
+    fn from(e: SdkError<ListObjectsV2Error>) -> Self {
+        ServerError::ListObjects { 
+            message: e.to_string(),
+        }
+    }
 }
 
 pub type Result<T> = std::result::Result<T, ServerError>;
